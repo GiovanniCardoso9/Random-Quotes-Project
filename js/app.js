d769dd032210ab1baa320157bc3afb8b90029c37
GiovanniCardoso9 <60896937+GiovanniCardoso9@users.noreply.github.com>
2020-03-19 18:07:53 -0300

    Update app.js

--- js/app.js
+++ js/app.js
@@ -2,18 +2,49 @@
   const quotes = [
     {
       quote:
+      "A violência resolve tudo",
+      author: "Katarina"
     },
     {
       quote:
+      "As vezes um coração frio só precisa de um sorriso quente",
+      author: "Braum"
     },
     {
       quote:
+      "O Verdadeiro Mestre é um eterno aprendiz",
+      author: "Master Yi"
     },
     {
       quote:
+      "Ficar parada é mais perigoso que seguir em frente",
+      author: "Talyah"
     },
     {
       quote:
+      "A dor é temporária, mas a vitória é eterna",
+      author: "Aatrox"
+    },
+      {
+        quote:
+        "Medo é o primeiro de muitos inimigos",
+        author: "Garen"
+      },
+      {
+        quote:
+        "Prefiro errar do que não fazer nada",
+        author: "Ekko"
+      }
+    ];
 
+    const btn = document.getElementById("generate-btn");
 
+    btn.addEventListener("click",function(){
+      let random = Math.floor(Math.random()*quotes.length);
+      console.log(random);
 
+      document.getElementById("quote").textContent = quote[random].quote;
+      document.querySelector(".author").textContent = quotes[random].author;
+
+    });
+  })();
